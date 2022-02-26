1fef43299c21ae8a287a0f21618142937b6e7cb4
jesusMelian <78150247+jesusMelian@users.noreply.github.com>
2022-02-26 12:14:14 +0000

    Features: add a option continue

--- src/com/jesusmelian/Main.java
+++ src/com/jesusmelian/Main.java
@@ -2,8 +2,10 @@ package com.jesusmelian;
 
 import com.jcraft.jsch.ChannelExec;
 import com.jcraft.jsch.JSch;
+import com.jcraft.jsch.JSchException;
 import com.jcraft.jsch.Session;
 
+import java.io.ByteArrayOutputStream;
 import java.util.Scanner;
 
 public class Main {
@@ -11,12 +13,14 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         final String ROUTE = "/var/log/";
+        boolean terminate = false;
 
         //ventana para escribir comandos
         Session session = null;
         //creamos un canal de ejecución
         ChannelExec channel = null;
 
+        //DATOS DE CONEXIÓN
         Scanner sc = new Scanner(System.in);
         System.out.println("Introduzca el nombre de usuario");
         final String USERNAME = sc.nextLine();
@@ -25,12 +29,14 @@ public class Main {
         System.out.println("Introduzca el puerto");
         final int PORT = sc.nextInt();
 
-
         sc.nextLine();
         System.out.println("Introduzca la contraseña");
         final String PASSWORD = sc.nextLine();
+
+
         try {
             if(USERNAME != null && HOST != null ) {
+                //CREO LA SESIÓN CON LOS PARÁMETROS
                 session = new JSch().getSession(USERNAME, HOST, PORT);
                 session.setPassword(PASSWORD);
                 session.setConfig("StrictHostKeyChecking", "no");
@@ -38,20 +44,54 @@ public class Main {
 
                 channel = (ChannelExec) session.openChannel("exec");
 
-                System.out.println("Inserte el nombre del archivo de registro");
-                String archive = sc.nextLine();
-
-                while(!comprobeArchive(archive)){
-                    System.out.println("El archivo debe terminar en .log");
+                while(!terminate) {
                     System.out.println("Inserte el nombre del archivo de registro");
-                    archive = sc.nextLine();
+                    String archive = sc.nextLine();
+
+                    //COMPRUEBO QUE EL ARCHIVO TERMINE EN .log
+                    while (!comprobeArchive(archive)) {
+                        System.out.println("El archivo debe terminar en .log");
+                        System.out.println("Inserte el nombre del archivo de registro");
+                        archive = sc.nextLine();
+                    }
+
+                    channel.setCommand("cd " + ROUTE + ";" + " cat " + archive);
+                    ByteArrayOutputStream responseStream = new ByteArrayOutputStream();
+                    channel.setOutputStream(responseStream);
+
+                    channel.connect();
+
+
+                    while (channel.isConnected()) {
+                        try {
+                            Thread.sleep(100);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+                    String responseString = new String(responseStream.toByteArray());
+
+                    System.out.println(responseString);
+
+                    System.out.println("¿DESEA CONTINUAR? S/N");
+                    String option = sc.nextLine();
+
+                    //Si introduce "N" o algo que no sea "S" el programa continuará
+                    if(!option.equalsIgnoreCase("S")){
+                        terminate = true;
+                    }
                 }
-
-                
-
             }
-        } catch (Exception e) {
-
+        } catch (JSchException e) {
+            e.printStackTrace();
+        } finally {
+            if( session != null){
+                session.disconnect();
+            }
+            if(channel != null){
+                channel.disconnect();
+            }
         }
     }
 
